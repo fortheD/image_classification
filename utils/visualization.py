32ee420d88544057c49c92669b7085c4bed57c23
leike <leike06507@hellobike.com>
2019-06-20 14:29:48 +0800

    modify vis

--- utils/visualization.py
+++ utils/visualization.py
@@ -8,21 +8,19 @@ from nets.model import ClassifyModel
 
 flags = tf.app.flags
 
-tf.flags.DEFINE_string('model_name', 'DenseNet121', 'The model you want to visualize')
+tf.flags.DEFINE_string('origin_model_path', '/tmp/train/VGG16.h5', 'Origin model path')
 tf.flags.DEFINE_string('saved_path', '/tmp/plotmodel/', 'The model image path')
 
 FLAGS = flags.FLAGS
 
 def main(_):
     tf.logging.set_verbosity(tf.logging.INFO)
-    model_name = FLAGS.model_name
 
-    classify_model = ClassifyModel(input_shape=(224,224,3), model_name=model_name, classes=1000, data_format="channels_last")
-    model = classify_model.keras_model()
+    model = tf.keras.models.load_model(FLAGS.origin_model_path)
 
     if not tf.gfile.IsDirectory(FLAGS.saved_path):
         tf.gfile.MakeDirs(FLAGS.saved_path)
-    SAVED_PATH = FLAGS.saved_path + model_name + ".png"
+    SAVED_PATH = FLAGS.saved_path + "model.png"
     tf.keras.utils.plot_model(model, SAVED_PATH)
     tf.logging.info('model plot complete')
     return
